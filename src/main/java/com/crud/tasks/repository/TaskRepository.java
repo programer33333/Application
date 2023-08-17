02d2af6aa4f895e4b02f457ca49e6db52657b3f5
Marcel <chlastawamarcel@gmail.com>
2023-08-17 12:11:28 +0200

    19.2

---
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -0,0 +1,16 @@
+package com.crud.tasks.repository;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Long> {
+    List<Task> findAll();
+
+    Optional<Task> findById(Long taskId);
+}
